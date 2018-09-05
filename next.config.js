67edf621fe0650425b1fa89306d6987c9e965c8e
Javier Carrillo Milla <stringparser@gmail.com>
2018-09-05 08:58:44 +0200

    fix: build assetPrefix

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const withTypescript = require('@zeit/next-typescript');
 console.log('NODE_ENV', process.env.NODE_ENV);
 
 exports = module.exports = withTypescript({
+  assetPrefix: process.env.NODE_ENV === 'production'
+    ? 'https://stringparser.github.io/crypto-exchange/docs/'
+    : '',
   pageExtensions: ['tsx', 'ts'],
 });
